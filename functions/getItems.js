c2afbd08ed7a0343451570ab7f5b77d21e0f5603
Abhishek <abhishekY495.sec@gmail.com>
2023-12-25 20:22:44 +0530

    Fix: added headers

--- functions/getItems.js
+++ functions/getItems.js
@@ -3,6 +3,10 @@ import { items } from "../items";
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+    },
     body: JSON.stringify(items),
   };
 };
--- functions/getSearchedItems.js
+++ functions/getSearchedItems.js
@@ -7,6 +7,10 @@ exports.handler = async (event, context) => {
   );
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+    },
     body: JSON.stringify(searchedItems),
   };
 };

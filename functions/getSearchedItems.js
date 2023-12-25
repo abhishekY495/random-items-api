e3c442b7af95520c12bd108bc2cb7d13c3f259a1
Abhishek <abhishekY495.sec@gmail.com>
2023-12-25 23:49:01 +0530

    Error handling

--- functions/getSearchedItems.js
+++ functions/getSearchedItems.js
@@ -2,15 +2,37 @@ import { items } from "../items";
 
 exports.handler = async (event, context) => {
   const query = event.queryStringParameters.query;
-  const searchedItems = items.filter((item) =>
-    item.name.toLowerCase().includes(query.toLowerCase())
-  );
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-    },
-    body: JSON.stringify(searchedItems),
-  };
+  if (query) {
+    if (query.trim().length !== 0) {
+      const searchedItems = items.filter((item) =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+      );
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+        },
+        body: JSON.stringify(searchedItems),
+      };
+    } else {
+      return {
+        statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+        },
+        body: JSON.stringify({ error: "Empty query parameter" }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+      },
+      body: JSON.stringify({ error: "Missing query parameter" }),
+    };
+  }
 };
